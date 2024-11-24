10dba9846cb98ffc9a21dbadf7abe96dd1e65de8
Lisa <lizamotorna2004@gmail.com>
2024-11-24 21:33:45 +0200

    finish stage 9 in task 2 about remove and restore images

--- task-2/js/main.js
+++ task-2/js/main.js
@@ -1,24 +1,49 @@
 "use strict";
-const imgs = document.getElementsByTagName("img");
-const amountTag = document.getElementById("amountOfImages");
-amountTag.innerText = imgs.length.toString();
+let imgs = Array.from(document.getElementsByTagName("img"));
 const date = document.getElementById("date");
-const actualDate = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}`;
-date.innerText = actualDate;
+const removedImages = [];
+imgs.forEach((img, index) => {
+    const storageImages = JSON.parse(localStorage.getItem("removedImages") || "[]");
+    if (storageImages.includes(index)) {
+        removedImages.push(index);
+        img.parentElement.style.display = "none";
+    }
+});
 const gallery = document.getElementsByClassName("gallery")[0];
 const closeBtn = gallery.children[0];
-for (const img of imgs) {
+const amountTag = document.getElementById("amountOfImages");
+amountTag.innerText = imgs.length.toString();
+const actualDate = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}`;
+date.innerText = actualDate;
+imgs.forEach((img, index) => {
     img.onclick = () => {
-        const path = img.getAttribute("src");
         gallery.classList.add("gallery-show");
         document.body.classList.add("overlay");
         const imgTagName = document.createElement("img");
-        imgTagName.src = path ? path : "";
+        imgTagName.src = img.getAttribute("src") || "";
         gallery.append(imgTagName);
     };
-}
+    const deleteBtn = img.nextElementSibling;
+    deleteBtn &&
+        (deleteBtn.onclick = () => {
+            img.parentElement.style.display = "none";
+            removedImages.push(index);
+            localStorage.setItem("removedImages", JSON.stringify(removedImages));
+            imgs = Array.from(document.getElementsByTagName("img"));
+            amountTag.innerText = imgs.length.toString();
+        });
+});
 closeBtn.onclick = () => {
     gallery.classList.remove("gallery-show");
     document.body.classList.remove("overlay");
     gallery.children[1].remove();
+};
+const restore = document.getElementsByClassName("restore")[0];
+restore.onclick = () => {
+    localStorage.removeItem("removedImages");
+    imgs.forEach((img, index) => {
+        const container = img.parentElement;
+        removedImages.length = 0;
+        container === null || container === void 0 ? void 0 : container.style.removeProperty("display");
+    });
 };
--- task-2/ts/main.ts
+++ task-2/ts/main.ts
@@ -1,11 +1,25 @@
-const imgs = document.getElementsByTagName(
-  "img"
-) as HTMLCollectionOf<HTMLImageElement>;
-const amountTag = document.getElementById("amountOfImages") as HTMLSpanElement;
-
-amountTag.innerText = imgs.length.toString();
-
+let imgs = Array.from(
+  document.getElementsByTagName("img")
+) as HTMLImageElement[];
 const date = document.getElementById("date") as HTMLDivElement;
+
+const removedImages: number[] = [];
+
+imgs.forEach((img: HTMLImageElement, index: number) => {
+  const storageImages: number[] = JSON.parse(
+    localStorage.getItem("removedImages") || "[]"
+  );
+  if (storageImages.includes(index)) {
+    removedImages.push(index);
+    (img.parentElement as HTMLDivElement).style.display = "none";
+  }
+});
+
+const gallery = document.getElementsByClassName("gallery")[0] as HTMLDivElement;
+const closeBtn = gallery.children[0] as HTMLButtonElement;
+
+const amountTag = document.getElementById("amountOfImages") as HTMLSpanElement;
+amountTag.innerText = imgs.length.toString();
 
 const actualDate = `${new Date().getDate()}.${
   new Date().getMonth() + 1
@@ -13,22 +27,45 @@ const actualDate = `${new Date().getDate()}.${
 
 date.innerText = actualDate;
 
-const gallery = document.getElementsByClassName("gallery")[0] as HTMLDivElement;
-const closeBtn = gallery.children[0] as HTMLButtonElement;
-
-for (const img of imgs) {
+imgs.forEach((img, index) => {
   img.onclick = () => {
-    const path = img.getAttribute("src");
     gallery.classList.add("gallery-show");
     document.body.classList.add("overlay");
-    const imgTagName = document.createElement("img");
-    imgTagName.src = path ? path : "";
+    const imgTagName = document.createElement("img") as HTMLImageElement;
+    imgTagName.src = img.getAttribute("src") || "";
     gallery.append(imgTagName);
   };
-}
+
+  const deleteBtn = img.nextElementSibling as HTMLButtonElement;
+
+  deleteBtn &&
+    (deleteBtn.onclick = () => {
+      (img.parentElement as HTMLDivElement).style.display = "none";
+      removedImages.push(index);
+      localStorage.setItem("removedImages", JSON.stringify(removedImages));
+      imgs = Array.from(
+        document.getElementsByTagName("img")
+      ) as HTMLImageElement[];
+      amountTag.innerText = imgs.length.toString();
+    });
+});
 
 closeBtn.onclick = () => {
-    gallery.classList.remove("gallery-show");
-    document.body.classList.remove("overlay");
-    gallery.children[1].remove();
-}
+  gallery.classList.remove("gallery-show");
+  document.body.classList.remove("overlay");
+  gallery.children[1].remove();
+};
+
+const restore = document.getElementsByClassName(
+  "restore"
+)[0] as HTMLButtonElement;
+
+restore.onclick = () => {
+  localStorage.removeItem("removedImages");
+
+  imgs.forEach((img, index) => {
+    const container = img.parentElement as HTMLDivElement;
+    removedImages.length = 0;
+    container?.style.removeProperty("display");
+  });
+};
