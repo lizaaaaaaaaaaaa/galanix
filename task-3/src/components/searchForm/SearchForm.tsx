9951c8fc1b2a0e4fa13c3110f52cceb83f0f63d0
Lisa <lizamotorna2004@gmail.com>
2024-11-25 15:15:57 +0200

    remove text from input if reset button clicked

--- task-3/src/components/searchForm/SearchForm.tsx
+++ task-3/src/components/searchForm/SearchForm.tsx
@@ -1,26 +1,40 @@
-import { FC, FormEvent } from "react";
+import { FC, FormEvent, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import styles from "./SearchForm.module.css";
 import search from "../../assets/images/svg/search.svg";
 
 const SearchForm: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState("");
+
+  const country = searchParams.get("country");
+
+  useEffect(() => {
+    if (!country) {
+      setInputValue("");
+    } else {
+      setInputValue(country);
+    }
+  }, [country]);
 
   const formSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const target = event.target as HTMLFormElement;
-    const input = target.elements[0] as HTMLInputElement;
-    const country: string = input.value.toLowerCase();
+    const country: string = inputValue.toLowerCase();
 
     if (country.length > 0) {
       setSearchParams({ country: country });
-      console.log(searchParams);
     }
   };
   return (
     <form onSubmit={formSubmitHandler} className={styles.form}>
-      <input type="text" placeholder="Enter country here..." minLength={1} />
+      <input
+        type="text"
+        placeholder="Enter country here..."
+        minLength={1}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <button>
         <img src={search} alt="button for form" />
       </button>
